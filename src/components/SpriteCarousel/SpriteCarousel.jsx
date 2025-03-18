86529df0de1df54ff6a546c8c2e065479db2726c
Miguel Torres <themtorres29@yahoo.com>
2025-03-17 17:14:40 -0700

    SpriteCarousel overflow Fixed

--- src/components/SpriteCarousel/SpriteCarousel.jsx
+++ src/components/SpriteCarousel/SpriteCarousel.jsx
@@ -1,24 +1,15 @@
 import './SpriteCarousel.css'
-import chienIcon from '../../assets/poke-icons/chien.webp'
-import mimikIcon from '../../assets/poke-icons/mimik.png'
-import dragIcon from '../../assets/poke-icons/drag.webp'
-import gholdIcon from '../../assets/poke-icons/ghold.webp'
-import flutterIcon from '../../assets/poke-icons/flutter.webp'
-import ironIcon from '../../assets/poke-icons/iron.webp'
-import zardIcon from '../../assets/poke-icons/zard.webp'
-import typhIcon from '../../assets/poke-icons/typh.webp'
-import gmolIcon from '../../assets/poke-icons/gmol.webp'
 
 const pokeSprites = [
-    {id: 1, src: chienIcon},
-    {id: 3, src: dragIcon},
-    {id: 4, src: gholdIcon},
-    {id: 5, src: flutterIcon},
-    {id: 6, src: ironIcon},
-    {id: 9, src: gmolIcon},
-    {id: 7, src: zardIcon},
-    {id: 8, src: typhIcon},
-    {id: 2, src: mimikIcon},
+    {id: 1, src: URL('../../assets/poke-icons/chien.webp')},
+    {id: 3, src: URL('../../assets/poke-icons/drag.webp')},
+    {id: 4, src: URL('../../assets/poke-icons/ghold.webp')},
+    {id: 5, src: URL('../../assets/poke-icons/flutter.webp')},
+    {id: 6, src: URL('../../assets/poke-icons/iron.webp')},
+    {id: 9, src: URL('../../assets/poke-icons/gmol.webp')},
+    {id: 7, src: URL('../../assets/poke-icons/zard.webp')},
+    {id: 8, src: URL('../../assets/poke-icons/typh.webp')},
+    {id: 2, src: URL('../../assets/poke-icons/mimik.png')},
 
 ]
 
